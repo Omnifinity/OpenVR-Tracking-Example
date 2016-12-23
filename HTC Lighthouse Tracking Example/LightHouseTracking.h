a12093b75cd5d6843f300226d9f6daf2aa88c819
Peter Thor <peter.thor@omnifinity.se>
2016-12-23 11:11:02 +0100

    Code cleanup - rearranging of some lines. Added boolean bWaitForEvents to allow dev to specity event parsing or continous parsing of tracking data.

--- HTC Lighthouse Tracking Example/LightHouseTracking.h
+++ HTC Lighthouse Tracking Example/LightHouseTracking.h
@@ -9,20 +9,24 @@
 class LighthouseTracking {
 private:
 
-	// basic stuff
+	// Basic stuff
 	vr::IVRSystem *m_pHMD = NULL;
 	vr::TrackedDevicePose_t m_rTrackedDevicePose[vr::k_unMaxTrackedDeviceCount];
 	Matrix4 m_rmat4DevicePose[vr::k_unMaxTrackedDeviceCount];
 
-	vr::HmdQuaternion_t LighthouseTracking::GetRotation(vr::HmdMatrix34_t matrix);
+	// Position and rotation of pose
 	vr::HmdVector3_t LighthouseTracking::GetPosition(vr::HmdMatrix34_t matrix);
+	vr::HmdQuaternion_t LighthouseTracking::GetRotation(vr::HmdMatrix34_t matrix);
+
+	// If false the program will parse tracking data continously and not wait for openvr events
+	bool bWaitForEventsBeforeParsing = false;
 
 public:
 	~LighthouseTracking();
 	LighthouseTracking();
 
 	// Main loop that listens for openvr events and calls process and parse routines, if false the service has quit
-	bool RunProcedure();
+	bool RunProcedure(bool bWaitForEvents);
 
 	// Process a VR event and print some general info of what happens
 	bool ProcessVREvent(const vr::VREvent_t & event);
